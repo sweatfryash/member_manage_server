315f0ea708d9f68db830300c31a831ecfd051454
hch <342254689@qq.com>
2023-07-12 09:29:21 +0800

    添加自动编译启动脚本

---
+++ startup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+program_name="bin/server.exe"
+webPath="$1"
+
+# 检查程序是否正在运行
+if pgrep -f "$program_name" >/dev/null; then
+  echo "正在停止 $program_name..."
+  pkill -f "$program_name"
+  sleep 1 # 可选：等待一段时间确保程序完全停止
+  echo "$program_name 已停止."
+fi
+
+# 更新程序源码
+echo "从gitee拉取源码"
+git pull gitee master
+
+# 重新编译和运行程序
+dart pub get
+dart compile exe bin/server.dart
+bin/server.dart --webAppDirectoryPath="$webPath"
+  echo "$program_name 已启动."
