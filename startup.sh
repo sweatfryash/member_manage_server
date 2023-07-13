d1438f50d6283a82f9999a1b1dd2a01b238214b5
hch <342254689@qq.com>
2023-07-13 17:23:26 +0800

    startup.sh添加libsqlite3-dev的检测

--- startup.sh
+++ startup.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# 检查 libsqlite3-dev 是否已安装
+if dpkg -s libsqlite3-dev >/dev/null 2>&1; then
+  echo "libsqlite3-dev is already installed."
+else
+  echo "libsqlite3-dev is not installed. Installing..."
+  sudo apt-get update
+  sudo apt-get install -y libsqlite3-dev
+  echo "libsqlite3-dev has been installed."
+fi
+
 program_name="bin/server.exe"
 webPath="$1"
 
@@ -12,7 +22,6 @@ if pgrep -f "$program_name" >/dev/null; then
 fi
 
 # 更新程序源码
-echo "拉取最新代码..."
 git pull
 
 # 重新编译和运行程序
